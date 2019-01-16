c0e3061533ffd45690c52ab9ccaf6c6e0ceffbe5
mateoguerra <mateo.guerra@ubiquotelemedicina.com>
2019-01-16 16:01:51 -0500

    A library for redux actions was added

---
+++ projects/addapptables/ngrx-actions/src/lib/redux-register-root.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, Injector, Inject, Optional, InjectFlags } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReducerManager } from '@ngrx/store';
+import { STORE_TOKEN } from './redux-tokens';
+import { createReducer } from './factory';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: []
+})
+export class ReduxRegisterRootModule {
+    constructor(@Optional() @Inject(STORE_TOKEN) reducers: any, reducerFactory: ReducerManager, parentInjector: Injector) {
+        if (reducers) {
+            // tslint:disable-next-line:forin
+            for (const key in reducers) {
+                const klass = reducers[key];
+                const inst = parentInjector.get(klass, new klass(), InjectFlags.Default);
+                reducerFactory.addReducer(key, createReducer(inst));
+            }
+        }
+    }
+}
---
+++ projects/addapptables/ngrx-actions/src/public_api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of ngrx-actions
+ */
+
+export { Action } from './lib/action';
+export { ofAction } from './lib/of-action';
+export { Store } from './lib/store';
+export { Select, NgrxSelect } from './lib/select';
+export { Effect } from './lib/effect';
+export { createReducer } from './lib/factory';
+export { ReduxRegisterModule } from './lib/redux-register.module';
